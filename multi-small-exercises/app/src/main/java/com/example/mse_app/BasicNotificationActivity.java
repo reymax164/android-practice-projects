2926c7f8c64c67a8266097bca539d86fc90cdd93
Reymax164 <reyesmaxine164@gmail.com>
2026-01-31 19:50:02 +0800

    MSE App
    - notification tap action

--- multi-small-exercises/app/src/main/java/com/example/mse_app/BasicNotificationActivity.java
+++ multi-small-exercises/app/src/main/java/com/example/mse_app/BasicNotificationActivity.java
@@ -1,13 +1,19 @@
 package com.example.mse_app;
 
+import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.core.graphics.Insets;
@@ -18,6 +24,7 @@ public class BasicNotificationActivity extends AppCompatActivity {
     Button notifyBtn;
     private final String CHANNEL_ID = "my_channel_01";
 
+    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,16 +35,31 @@ public class BasicNotificationActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        
+        final int NOTIFICATION_PERMISSION_CODE = 123;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.POST_NOTIFICATIONS},
+                    NOTIFICATION_PERMISSION_CODE);
+            return;
+        }
 
         notifyBtn = findViewById(R.id.btnNotify);
         createNotificationChannel();
 
+        Intent intent = new Intent(this, NotificationDetailsActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
         notifyBtn.setOnClickListener(v -> {
             NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.mipmap.ic_launcher_nash)
-                .setContentTitle("Hello this is Nashley")
-                .setContentText("This is the notification you clicked.")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                    .setSmallIcon(R.mipmap.ic_launcher_nash)
+                    .setContentTitle("Hello this is MSE App")
+                    .setContentText("This is the notification you clicked.")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true);
 
             NotificationManagerCompat.from(this).notify(1, builder.build());
         });
